ce0dfbff4a7953de0c61d64bf5908c869f873f2a
Danny Coates <dannycoates@gmail.com>
2011-02-20 17:25:07 -0700

    stubbed client api

--- test/client-test.js
+++ test/client-test.js
@@ -1,72 +1,94 @@
 var assert = require('assert'),
-		Client = require('../lib/client').Client;
+    Client = require('../lib/client').Client;
 
 var createClient = function() {
-	var client = new Client();
-	client.connect(5858);
-	return client;
+  var client = new Client();
+  client.connect(5858);
+  return client;
 }
 
 exports['client emits "ready" when a message with "connect" type is received'] =
-	function() {
-		var client = new Client();
-		var tid = setTimeout(function() {
-			assert.ok(false, "didn't receive 'ready'");
-		}, 100);
-		client.on('ready', function() {
-			clearTimeout(tid);
-		});
-		client.handleMessage({
-			headers: { Type: 'connect' },
-			body: {}
-		});
-	}
+  function() {
+    var client = new Client();
+    var tid = setTimeout(function() {
+      assert.ok(false, "didn't receive 'ready'");
+    }, 100);
+    client.on('ready', function() {
+      clearTimeout(tid);
+    });
+    client.handleMessage({
+      headers: { Type: 'connect' },
+      body: {}
+    });
+  }
 
-
+/* Version response body
+ * body: { "V8Version" : "" }
+ */
 exports['client can get version info from host'] = function() {
-	var client = new Client();
-	client.write = function() {};
-	var tid = setTimeout(function() {
-			assert.ok(false, "didn't get version");
-		}, 100);
+  var client = new Client();
+  client.write = function() {};
+  var tid = setTimeout(function() {
+      assert.ok(false, "didn't get version");
+    }, 100);
 
-	client.version(function(v) {
-		clearTimeout(tid);
-		assert.ok(/\d+\.\d+\.\d+/.test(v));
-		assert.strictEqual(client.callbacks.length, 0);
-	});
-	client.handleMessage({
-		headers: {},
-		body: { 
-			type: 'response',
-			request_seq: 1,
-			success: true,
-			running: true,
-			body: { V8Version: '3.2.1'}}
-	});
+  client.version(function(v) {
+    clearTimeout(tid);
+    assert.ok(/\d+\.\d+\.\d+/.test(v));
+  });
+  client.handleMessage({
+    headers: {},
+    body: { 
+      type: 'response',
+      request_seq: 1,
+      success: true,
+      running: true,
+      body: { V8Version: '3.2.1'}}
+  });
 }
 
+/* Scripts response body
+ * body:
+ *
+ * [ { "name" :
+ *      "id" :
+ *      "lineOffset" :
+ *      "columnOffset" :
+ *      "lineCount" :
+ *      "data" :
+ *      "source" :
+ *      "sourceStart" :
+ *      "sourceLength" :
+ *      "scriptType" :
+ *      "compilationType" :
+ *      "evalFromScript" :
+ *      "evalFromLocation" :
+ *      }]
+ */
 exports['client can get the list of scripts from the host'] = function() {
-	var client = new Client();
-	client.write = function() {};
-	var tid = setTimeout(function() {
-			assert.ok(false, "didn't get scripts");
-		}, 100);
-	var script = { id: 1, handle: 2, type: 'script', name: 'test.js' };
+  var client = new Client();
+  client.write = function() {};
+  var tid = setTimeout(function() {
+      assert.ok(false, "didn't get scripts");
+    }, 100);
+  var script = { id: 1, handle: 2, type: 'script', name: 'test.js' };
 
-	client.getScripts(function() {
-		clearTimeout(tid);
-		assert.strictEqual(client.handles[2], script);
-		assert.strictEqual(client.scripts[1], script);
-	});
-	client.handleMessage({
-		headers: {},
-		body: {
-			type: 'response',
-			request_seq: 1,
-			success: true,
-			running: true,
-			body: [ script ]
-		}
-	});
+  client.getScripts(function(scripts) {
+    clearTimeout(tid);
+    assert.strictEqual(scripts[0], script);
+  });
+  client.handleMessage({
+    headers: {},
+    body: {
+      type: 'response',
+      request_seq: 1,
+      success: true,
+      running: true,
+      body: [ script ]
+    }
+  });
+}
+
+exports['client can set and clear breakpoints'] = function() {
+
 }
--- test/protocol-test.js
+++ test/protocol-test.js
@@ -1,6 +1,6 @@
 var assert = require('assert'),
-		Buffer = require('buffer').Buffer,
-		Protocol = require('../lib/protocol').Protocol;
+    Buffer = require('buffer').Buffer,
+    Protocol = require('../lib/protocol').Protocol;
 
 var message = '{"seq":1,"type":"response","request_seq":1,"command":"v8flags","running":true,"success":true}';
 var request = '{"seq":1,"type":"request","command":"v8flags","arguments":{"flags":"--t®ace_gc"}}';
@@ -9,62 +9,50 @@ var packet = header + message;
 
 
 exports['appending a message in many chunks emits the message once'] = function() {
-	var protocol = new Protocol();
+  var protocol = new Protocol();
 
-	protocol.on('message', function(msg) {
-		assert.strictEqual(JSON.stringify(msg.body), message);
-	});
-	protocol.append(packet.slice(0,5));
-	protocol.append(packet.slice(5, 34));
-	protocol.append(packet.slice(34, 45));
-	protocol.append(packet.slice(45, 56));
-	protocol.append(packet.slice(56) + 'foo');
+  protocol.on('message', function(msg) {
+    assert.strictEqual(JSON.stringify(msg.body), message);
+  });
+  protocol.append(packet.slice(0,5));
+  protocol.append(packet.slice(5, 34));
+  protocol.append(packet.slice(34, 45));
+  protocol.append(packet.slice(45, 56));
+  protocol.append(packet.slice(56) + 'foo');
 
 }
 
 exports['protocol can parse multibyte utf8 characters'] = function() {
-	var protocol = new Protocol();
-	var utf = '{"®12":true}';
-	protocol.on('message', function(msg) {
-		assert.strictEqual(JSON.stringify(msg.body), utf);
-	});
-	protocol.append('Content-Length: 13\r\n\r\n');
-	protocol.append(utf + 'XXX');
+  var protocol = new Protocol();
+  var utf = '{"®12":true}';
+  protocol.on('message', function(msg) {
+    assert.strictEqual(JSON.stringify(msg.body), utf);
+  });
+  protocol.append('Content-Length: 13\r\n\r\n');
+  protocol.append(utf + 'XXX');
 }
 
 exports['appending two messages at once will emit two messages'] = function() {
-	var protocol = new Protocol(),
-			count = 0;
+  var protocol = new Protocol(),
+      count = 0;
 
-	protocol.on('message', function(msg) {
-		assert.strictEqual(JSON.stringify(msg.body), message);
-		count++;
-	});
-	setTimeout(function() {
-		assert.strictEqual(count, 2);
-	}, 100);
+  protocol.on('message', function(msg) {
+    assert.strictEqual(JSON.stringify(msg.body), message);
+    count++;
+  });
+  setTimeout(function() {
+    assert.strictEqual(count, 2);
+  }, 100);
 
-	protocol.append(packet + packet);
+  protocol.append(packet + packet);
 }
 
 exports['the Content-Length of a serialized message is correct'] = function() {
-	var protocol = new Protocol();
+  var protocol = new Protocol();
 
-	var json = JSON.parse(request);
-	var serial = protocol.serialize(json);
-	var byteLength = +(/Content-Length: (\d+)/.exec(serial)[1]);
-	assert.strictEqual(byteLength, Buffer.byteLength(request));
+  var json = JSON.parse(request);
+  var serial = protocol.serialize(json);
+  var byteLength = +(/Content-Length: (\d+)/.exec(serial)[1]);
+  assert.strictEqual(byteLength, Buffer.byteLength(request));
 }
 
-exports['the seq of serialized messages increments by 1'] = function() {
-	var protocol = new Protocol();
-
-	var json = JSON.parse(request);
-	var serial1 = protocol.serialize(json);
-	var serial2 = protocol.serialize(json);
-
-	var seq1 = +(/seq..(\d+)/.exec(serial1)[1]);
-	var seq2 = +(/seq..(\d+)/.exec(serial2)[1]);
-
-	assert.strictEqual(seq1, seq2 - 1);
-}

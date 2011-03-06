f16501ae78e6eaf506569baa59f3ca0fcd47e73e
Danny Coates <dannycoates@gmail.com>
2011-03-06 00:05:13 -0700

    implemented watches on breakpoints

--- test/client-test.js
+++ test/client-test.js
@@ -32,8 +32,9 @@ exports['client can get version info from host'] = function() {
       assert.ok(false, "didn't get version");
     }, 100);
 
-  client.version(function(v) {
+  client.version(function(e, v) {
     clearTimeout(tid);
+    assert.equal(e, undefined);
     assert.ok(/\d+\.\d+\.\d+/.test(v));
   });
   client.handleMessage({
@@ -73,8 +74,9 @@ exports['client can get the list of scripts from the host'] = function() {
     }, 100);
   var script = { id: 1, handle: 2, type: 'script', name: 'test.js' };
 
-  client.getScripts(function(scripts) {
+  client.getScripts(function(e, scripts) {
     clearTimeout(tid);
+    assert.equal(e, undefined);
     assert.strictEqual(scripts[0], script);
   });
   client.handleMessage({
